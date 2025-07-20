6fd91fceed05f883bcc51bdc2b0e1ee1dc90c7f5
Varundhyani69 <varundhyani29@gmail.com>
2025-07-20 16:56:36 +0530

    updated cors for render

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT;
 mongoose.connect(process.env.MONGO_URI).then(() => { console.log("DB connected") }).catch((err) => { console.log(err) });
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://uni-meet-eta.vercel.app'
+    'https://uni-meet-eta.vercel.app',
+    'https://unimeet-frontend.onrender.com'
 ];
 app.use(cookieParser());
 app.use(cors({
