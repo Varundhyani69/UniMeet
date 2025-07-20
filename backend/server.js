af436fe27d09e51657e1ac174cb686b2a143b947
Varundhyani69 <varundhyani29@gmail.com>
2025-07-20 19:26:35 +0530

    referesh issues

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,10 @@ app.use('/api/user', userRoute);
 app.use('/api/message', messageRoutes);
 app.use('/api/timetable', timetableRoutes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.get('/', (req, res) => {
     res.send("Backend is running");
 });
