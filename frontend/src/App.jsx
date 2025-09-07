07f4fa953090cd70fd8420faefaff1da9e0017b4
Varundhyani69 <varundhyani29@gmail.com>
2025-09-07 23:45:15 +0530

    Admin dev started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,19 @@ import RegisterLogin from './components/RegisterLogin';
 import Dashboard from './components/Dashboard';
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-
-
   return (
     <>
       <Routes>
         <Route path='/' element={<RegisterLogin />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        {/* <Route path='/admin' element={<div>Admin Dashboard (To be implemented by you)</div>} /> for you */}
       </Routes>
       <ToastContainer position="top-right" autoClose={3000} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
